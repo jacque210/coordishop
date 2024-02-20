568bfb0ae8597e76da588b3fbd9e65bd0f1c1dc6
dreamer210 <ohlala210@gmail.com>
2024-02-20 19:28:10 +0900

    feat: logging, jpa, mockk 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	idea
+	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.22"
@@ -26,8 +28,12 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 	implementation("com.h2database:h2")
+	implementation("org.bgee.log4jdbc-log4j2:log4jdbc-log4j2-jdbc4.1:1.16")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.8")
 }
 
 tasks.withType<KotlinCompile> {
