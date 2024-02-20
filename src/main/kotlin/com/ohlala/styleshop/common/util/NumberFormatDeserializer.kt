269f314fe8b63538b3ea8b68c22e08968bbe3cba
dreamer210 <ohlala210@gmail.com>
2024-02-20 19:31:24 +0900

    chore: 불필요 코드 정리

--- src/main/kotlin/com/ohlala/styleshop/common/util/NumberFormatDeserializer.kt
+++ src/main/kotlin/com/ohlala/styleshop/common/util/NumberFormatDeserializer.kt
@@ -3,7 +3,6 @@ package com.ohlala.styleshop.common.util
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
-import java.text.NumberFormat
 
 class NumberFormatDeserializer : JsonDeserializer<String>() {
   override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): String {
